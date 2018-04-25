dbfe6d0a7a210cf0db5fd4412973bbf55fee13ac
Edit Xie <34528608+EditXie@users.noreply.github.com>
2018-04-25 23:46:05 +0800

    finish markbook

---
+++ account/urls.py
@@ -0,0 +1,54 @@
+from django.conf.urls import url
+from account.views import *
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change_done
+from django.contrib.auth.views import password_reset
+from django.contrib.auth.views import password_reset_done
+from django.contrib.auth.views import password_reset_confirm
+from django.contrib.auth.views import password_reset_complete
+
+urlpatterns = [
+    # post views
+    # # url(r'^login/$', user_login, name='login'),
+    # url(r'^login/$',
+    #     'django.contrib.auth.views.login',
+    #     name='login'),
+    # url(r'^logout/$',
+    #     'django.contrib.auth.views.logout',
+    #     name='logout'),
+    # url(r'^logout-then-login/$',
+    #     'django.contrib.auth.views.logout_then_login',
+    #     name='logout_then_login'),
+    url(r'^$', dashboard, name='dashboard'),
+
+    # login 和logout
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+    # change password
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+
+    # 重设密码和恢复密码
+    url(r'^password-reset/$',
+        password_reset,
+        name='password_reset'),
+    url(r'^password-reset/done/$',
+        password_reset_done,
+        name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$',
+        password_reset_complete,
+        name='password_reset_complete'),
+
+    # 用户注册
+    url(r'^register/$', register, name='register'),
+    # 用户编辑
+    url(r'^edit/$', edit, name='edit'),
+
+]
